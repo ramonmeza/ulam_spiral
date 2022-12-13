5a3faa62b42c225ccbec1e04534dd77ac8aec27d
Ramon Meza <ramon_meza@live.com>
2022-12-13 12:31:22 -0500

    Add is_prime()

--- include/ulam/ulam.hh
+++ include/ulam/ulam.hh
@@ -1,3 +1,5 @@
+#include <string>
+
 namespace ulam
 {
     const int RIGHT = 0;
@@ -5,6 +7,33 @@ namespace ulam
     const int LEFT = 2;
     const int DOWN = 3;
 
+    enum TextColor
+    {
+        black = 30,
+        red,
+        green,
+        yellow,
+        blue,
+        magenta,
+        cyan,
+        white
+    };
+
+    enum TextStyle
+    {
+        reset = 0,
+        bold = 1,
+        bright = 1,
+        underline = 4,
+        inverse = 7,
+        bold_off = 21,
+        bright_off = 21,
+        underline_off = 24,
+        iinverse_off = 27
+    };
+
+    bool is_prime(int number);
+    void print_colored_text(const std::string &text, TextColor color, TextStyle style = TextStyle::reset);
     void print_table(int data[], int size, int max_cols);
     int *get_value(int data[], int width, int x, int y);
 }
--- src/ulam/ulam.cc
+++ src/ulam/ulam.cc
@@ -1,5 +1,33 @@
+#include <cmath>
 #include <iostream>
 #include <ulam/ulam.hh>
+
+bool ulam::is_prime(int number)
+{
+    // number must be greater than 1
+    if (number <= 1)
+        return false;
+
+    // 2 is the only even prime number
+    if (number == 2)
+        return true;
+    if (number % 2 == 0)
+        return false;
+
+    // if a number is divisible by any number less than itself (down to 2)
+    // than the number is not prime
+    for (unsigned int i = (unsigned int)sqrt(number); i > 2; i--)
+    {
+        if (number % i == 0)
+            return false;
+    }
+    return true;
+}
+
+void ulam::print_colored_text(const std::string &text, ulam::TextColor color, TextStyle style)
+{
+    std::cout << "\e[" << (int)style << ";" << (int)color << "m" << text << "\e[0m";
+}
 
 void ulam::print_table(int data[], int size, int max_cols)
 {
